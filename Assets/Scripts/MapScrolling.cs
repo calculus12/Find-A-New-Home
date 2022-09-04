541e59a939868a35d3a49f5bcd3c80dc6a9aa6cd
calculus12 <55823958+calculus12@users.noreply.github.com>
2022-09-04 22:24:08 +0900

    fix: 누락 스크립트 추가

---
+++ Assets/Scripts/MapScrolling.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 뒤로 이동한 배경을 앞으로 재배치하는 스크립트 (About z)
+public class MapScrolling : MonoBehaviour
+{
+    [SerializeField] float endPosition;
+    [SerializeField] float startPosition;
+
+    [SerializeField] List<Transform> children;
+
+    [SerializeField] float speed;
+
+    private void Awake()
+    {
+        // 자식 트랜스폼 가져오기
+        foreach (Transform child in transform)
+        {
+            Debug.Log(child.name);
+            children.Add(child);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // 현재 z위치가 endPosition 이하로 이동했을때 위치를 리셋
+        foreach (Transform transform in children)
+        {
+            transform.Translate(Vector3.back * speed);
+            if (transform.position.z <= endPosition)
+            {
+            Reposition(transform);
+            }
+        }
+    }
+
+    // 위치를 리셋하는 메서드
+    private void Reposition(Transform transform)
+    {
+        // 현재 z위치를 startPosition으로 리셋
+        transform.position = new Vector3(transform.position.x, transform.position.y, startPosition);
+    }
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other){
+        //player die function
+        if(other.tag == "Obstacle"){
+            Debug.Log("충돌발생");
+        }
+        else if (other.tag == "Coin") {
+            MovingObjectPool.instance.ReturnObj(other.GetComponent<Coin>());
+        }
+    }
+}
