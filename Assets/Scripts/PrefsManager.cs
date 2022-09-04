14aebd880e03059637615a0291ae34cc917f02ea
calculus12 <55823958+calculus12@users.noreply.github.com>
2022-09-05 00:24:40 +0900

    feat: 로컬 데이터 저장을 위한 Prefs Manager 작성

--- Assets/Scripts/PrefsManager.cs
+++ Assets/Scripts/PrefsManager.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class PrefsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static PrefsManager _instance;
+
+    public static PrefsManager Instance
     {
-        
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("Prefs Manger instance is null");
+            }
+
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
+
+    public int GetCoin()
+    {
+        return PlayerPrefs.GetInt("coin", 0);
+    }
+
+    public void SetCoin(int coin)
+    {
+        if (coin < 0) return;
+        PlayerPrefs.SetInt("coin", coin);
+    }
+
+    public int GetBestScore()
+    {
+        return PlayerPrefs.GetInt("best_score", 0);
+    }
+
+    public void SetBestScore(int bestScore) 
+    {
+        if (bestScore < 0) return;
+        PlayerPrefs.SetInt("best_score", bestScore);
+    }
+
 }
