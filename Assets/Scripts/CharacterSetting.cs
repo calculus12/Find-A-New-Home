fd25673756cf467170fdf9f56b3ce482b2d8d3f6
calculus12 <55823958+calculus12@users.noreply.github.com>
2022-09-05 22:02:09 +0900

    feat: 게임오버 애니메이션 추가

--- Assets/Scripts/CharacterSetting.cs
+++ Assets/Scripts/CharacterSetting.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CharacterSetting : MonoBehaviour
 {
     string characterName = "Penguin";
+    [SerializeField] GameObject gameoverCharacters;
     void Start()
     {
         // 캐릭터 이름을 전달받고 파괴시킨다
@@ -12,7 +13,10 @@ public class CharacterSetting : MonoBehaviour
 
         // 선택된 캐릭터를 활성화
         var character = transform.Find(characterName).gameObject;
+        // 게임오버 캐릭터 활성화
+        var goCharacter = gameoverCharacters.transform.Find(characterName).gameObject;
         //Debug.Log(characterName);
         character.SetActive(true);
+        goCharacter.SetActive(true);
     }
 }
